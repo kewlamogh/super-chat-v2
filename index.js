cce4732c87628b3c3287c1899b85f57713c6d797
DaCool1 <86981743+kewlamogh@users.noreply.github.com>
2021-08-25 19:34:26 -0700

    moved commented Crypto to `experimental/crypto.`js

--- index.js
+++ index.js
@@ -9,41 +9,7 @@ let currentUser = "Anonymous";
 let rooms = [];
 const Database = require("@replit/database");
 const db = new Database()
-/*
-const crypto = require("crypto")
 
-const encrypt = (plainText) => {
-  try {
-    const iv = crypto.randomBytes(16);
-    const key = crypto.createHash('sha256').update(process.env.password).digest('base64').substr(0, 32);
-    const cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-
-    let encrypted = cipher.update(plainText);
-    encrypted = Buffer.concat([encrypted, cipher.final()])
-    return iv.toString('hex') + ':' + encrypted.toString('hex');
-
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-const decrypt = (encryptedText) => {
-  try {
-    const textParts = encryptedText.split(':');
-    const iv = Buffer.from(textParts.shift(), 'hex');
-
-    const encryptedData = Buffer.from(textParts.join(':'), 'hex');
-    const key = crypto.createHash('sha256').update(process.env.password).digest('base64').substr(0, 32);
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-    
-    const decrypted = decipher.update(encryptedData);
-    const decryptedText = Buffer.concat([decrypted, decipher.final()]);
-    return decryptedText.toString();
-  } catch (error) {
-    console.log(error)
-  }
-}
-*/
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/login.html");
